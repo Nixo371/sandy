3357d21cdb941eefda696ebe6a6bd97d2c8bf50d
Nicolas Ucieda <nicolas.ucieda@gmail.com>
2026-01-03 19:53:52 +0100

    All particles are updated from the bottom to the top of the screen now

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@
 #define WIDTH 900
 #define HEIGHT 600
 
-#define FPS 60
+#define FPS 240
 #define FRAME_TIME_MS (1000 / FPS)
 
 typedef enum e_particle_type {
@@ -31,10 +31,6 @@ typedef struct s_particle {
 
 typedef struct s_game_state {
 	particle** board;
-
-	int sand_capacity;
-	int sand_count;
-	particle* sand;
 } game_state;
 
 int in_bounds(int x, int y) {
@@ -61,27 +57,11 @@ game_state* init_game_state() {
 		}
 	}
 
-	state->sand_capacity = 128;
-	state->sand_count = 0;
-	state->sand = calloc(state->sand_capacity, sizeof(particle));
-
 	return (state);
 }
 
 void add_sand_particle(game_state* state, int x, int y) {
-	if (state->sand_count >= state->sand_capacity) {
-		state->sand_capacity *= 2;
-		state->sand = (particle *) realloc(state->sand, state->sand_capacity * sizeof(particle));
-	}
-
 	state->board[x][y].type = SAND;
-
-	state->sand[state->sand_count].x = x;
-	state->sand[state->sand_count].y = y;
-	state->sand[state->sand_count].type = SAND;
-
-
-	state->sand_count += 1;
 }
 
 void free_game_state(game_state* state) {
@@ -91,18 +71,10 @@ void free_game_state(game_state* state) {
 	free(state->board);
 	state->board = NULL;
 
-	state->sand_capacity = 0;
-	state->sand_count = 0;
-	free(state->sand);
-	state->sand = NULL;
-
 	free(state);
 }
 
-void update_particle(game_state* state, particle* p) {
-	int x = p->x;
-	int y = p->y;
-
+void update_particle(game_state* state, int x, int y) {
 	switch (state->board[x][y].type) {
 		case AIR:
 			break;
@@ -110,32 +82,26 @@ void update_particle(game_state* state, particle* p) {
 			if (in_bounds(x, y + 1) && state->board[x][y + 1].type == AIR) {
 				state->board[x][y].type = AIR;
 				state->board[x][y + 1].type = SAND;
-
-				p->x = x;
-				p->y = y + 1;
 			}
 			else if (in_bounds(x - 1, y + 1) && state->board[x - 1][y + 1].type == AIR) {
 				state->board[x][y].type = AIR;
 				state->board[x - 1][y + 1].type = SAND;
-
-				p->x = x - 1;
-				p->y = y + 1;
 			}
 			else if (in_bounds(x + 1, y + 1) && state->board[x + 1][y + 1].type == AIR) {
 				state->board[x][y].type = AIR;
 				state->board[x + 1][y + 1].type = SAND;
-
-				p->x = x + 1;
-				p->y = y + 1;
 			}
 
 			break;
 	}
 }
 
-void update_sand_particles(game_state* state) {
-	for (int i = 0; i < state->sand_count; i++) {
-		update_particle(state, &state->sand[i]);
+void update_all_particles(game_state* state) {
+	// Update from down up
+	for (int y = HEIGHT - 1; y >= 0; y--) {
+		for (int x = 0; x < WIDTH; x++) {
+			update_particle(state, x, y);
+		}
 	}
 }
 
@@ -181,14 +147,19 @@ int main() {
 		SDL_SetRenderDrawColor(main_renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
 		SDL_RenderClear(main_renderer);
 
-		SDL_SetRenderDrawColor(main_renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-		for (int i = 0; i < state->sand_count; i++) {
-			SDL_RenderPoint(main_renderer, state->sand[i].x, state->sand[i].y);
+		// #FFD000 "Gold Web" sand-ish color
+		SDL_SetRenderDrawColor(main_renderer, 255, 208, 0, SDL_ALPHA_OPAQUE);
+		for (int x = 0; x < WIDTH; x++) {
+			for (int y = 0; y < HEIGHT; y++) {
+				if (state->board[x][y].type == SAND) {
+					SDL_RenderPoint(main_renderer, x, y);
+				}
+			}
 		}
 
 		SDL_RenderPresent(main_renderer);
 
-		update_sand_particles(state);
+		update_all_particles(state);
 		SDL_Delay(FRAME_TIME_MS);
 	}
 
