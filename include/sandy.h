1992732cfd3ce605b4c9862d2904004d062989c6
Nicolas Ucieda <nicolas.ucieda@gmail.com>
2026-02-10 21:16:50 +0100

    feat: particles are now data driven

--- include/sandy.h
+++ include/sandy.h
@@ -3,13 +3,18 @@
 
 // Forward Declarations //
 typedef struct s_particle particle;
+typedef struct s_particle_type particle_type;
 // -------------------- //
 
 typedef struct s_game_state {
 	particle** board;
+        int particle_type_count;
+        particle_type* particle_types;
+
 	int blob_radius;
 } game_state;
 
 int in_bounds(int x, int y);
+particle_type* get_particle_type(game_state* state, char id);
 
 #endif
--- src/sandy.c
+++ src/sandy.c
@@ -1,6 +1,8 @@
 #include "sandy.h"
+#include "particle.h"
 
 #include "constants.h"
+#include <unistd.h>
 
 int in_bounds(int x, int y) {
 	if (x < 0 || x >= WIDTH) {
@@ -13,3 +15,12 @@ int in_bounds(int x, int y) {
 	return (1);
 }
 
+particle_type* get_particle_type(game_state* state, char id) {
+        for (int i = 0; i < state->particle_type_count; i++) {
+                if (state->particle_types[i].id == id) {
+                        return (&state->particle_types[i]);
+                }
+        }
+
+        return (NULL);
+}
